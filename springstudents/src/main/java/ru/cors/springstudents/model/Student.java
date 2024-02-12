a4e877aadd3749db884437b8ccab388a976c942b
Artem Khrapuzov <tema102@mail.ru>
2024-02-12 19:12:46 +0600

    create update delete find. REST app

---
+++ springstudents/src/main/java/ru/cors/springstudents/model/Student.java
@@ -0,0 +1,31 @@
+package ru.cors.springstudents.model;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+@Data
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    @Column(unique = true)
+    private String email;
+    @Transient
+    private int age;
+
+    public int getAge(){
+        return Period.between(dateOfBirth, LocalDate.now()).getYears();
+    }
+
+
+
+
+}
---
+++ springstudents/src/main/java/ru/cors/springstudents/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package ru.cors.springstudents.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.cors.springstudents.model.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    void deleteByEmail(String email);
+    Student findStudentByEmail(String email);
+}
